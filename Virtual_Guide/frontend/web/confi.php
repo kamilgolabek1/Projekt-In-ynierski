1aa057766738f25033c37a5556b3c199bbe926f7
cessla <the21@interia.pl>
2016-03-18 14:29:51 +0100

    usunięcie śmieci

--- Virtual_Guide/frontend/web/confi.php
+++
@@ -1,5 +0,0 @@
-<?php
-$con = mysqli_connect('127.0.0.1', 'root', 'pass') or die('Could not connect the database : Username or password incorrect');
-$sel = mysqli_select_db($con, 'virtualguide') or die ('No database found');
-
-?>
